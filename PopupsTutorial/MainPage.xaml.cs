a036ba45bbc77f436f665ba156633b4db49515f0
Дмитрий Севрюков <sevryukov.d2003@gmail.com>
2021-12-08 10:51:20 +0300

    создал MinPage, заменил в нём код открыл MainPage.xaml.cs и заменил код

---
+++ PopupsTutorial/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PopupsTutorial
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnDisplayAlertButtonClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert", "This is an alert.", "OK");
+        }
+
+        async void OnDisplayAlertQuestionButtonClicked(object sender, EventArgs e)
+        {
+            bool response = await DisplayAlert("Save?", "Would you like to save your data?", "Yes", "No");
+            Console.WriteLine("Save data: " + response);
+        }
+    }
+}
