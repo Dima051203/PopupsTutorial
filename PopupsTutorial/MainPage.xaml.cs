382dc560c297816a5a41eb3074b0ee9c36fd54d3
Дмитрий Севрюков <sevryukov.d2003@gmail.com>
2021-12-08 10:56:58 +0300

    добавил обработчик решений в MainPage

--- PopupsTutorial/MainPage.xaml.cs
+++ PopupsTutorial/MainPage.xaml.cs
@@ -27,5 +27,10 @@ namespace PopupsTutorial
             bool response = await DisplayAlert("Save?", "Would you like to save your data?", "Yes", "No");
             Console.WriteLine("Save data: " + response);
         }
+        async void OnDisplayActionSheetButtonClicked(object sender, EventArgs e)
+        {
+            string action = await DisplayActionSheet("Send to?", "Cancel", null, "Email", "Twitter", "Facebook");
+            Console.WriteLine("Action: " + action);
+        }
     }
 }
